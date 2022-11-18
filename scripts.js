6a83f5e3ebdfc286d76b900ed48b2595480d8de8
schmidward <eric.d.schmid1@gmail.com>
2022-11-17 19:03:50 -0600

    Added update to flight status on click.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener('load', function(){
-    //Add a window asking the user to confirm the shuttle is ready for takeoff
     const takeoff = document.querySelector('#takeoff');
+    const flightStatus = document.getElementById('flightStatus');
 
+    //Add a window asking the user to confirm the shuttle is ready for takeoff
     takeoff.addEventListener('click', function(){
         let takeoffResponse = confirm("Confirm that the shuttle is ready for takeoff");
         console.log(`The user selected: ${takeoffResponse}`);
+        
+        //Update flight status to shuttle in flight
+        if (takeoffResponse) {
+            flightStatus.innerHTML ='Shuttle in flight.';
+        }
     });
+    
+
 });
 
