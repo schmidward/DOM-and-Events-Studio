8e3880176e3779a3002f4d85c45849b137816640
schmidward <eric.d.schmid1@gmail.com>
2022-11-17 19:11:53 -0600

    Shuttle background color updates to blue on takeoff click

--- scripts.js
+++ scripts.js
@@ -3,18 +3,20 @@
 window.addEventListener('load', function(){
     const takeoff = document.querySelector('#takeoff');
     const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.querySelector('#shuttleBackground');
 
     //Add a window asking the user to confirm the shuttle is ready for takeoff
     takeoff.addEventListener('click', function(){
         let takeoffResponse = confirm("Confirm that the shuttle is ready for takeoff");
-        console.log(`The user selected: ${takeoffResponse}`);
-        
-        //Update flight status to shuttle in flight
+                
+
         if (takeoffResponse) {
+            //Update flight status to shuttle in flight
             flightStatus.innerHTML ='Shuttle in flight.';
+            shuttleBackground.style.backgroundColor = 'blue'; 
         }
     });
-    
+    console.dir(shuttleBackground);
 
 });
 
