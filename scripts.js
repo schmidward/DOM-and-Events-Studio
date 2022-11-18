e664ee0dd4165d1a6e4d73a20ca2e3abf6d585d2
schmidward <eric.d.schmid1@gmail.com>
2022-11-17 18:56:03 -0600

    Added window to confirm shuttle is ready for takeoff

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener('load', function(){
+    //Add a window asking the user to confirm the shuttle is ready for takeoff
+    const takeoff = document.querySelector('#takeoff');
 
+    takeoff.addEventListener('click', function(){
+        let takeoffResponse = confirm("Confirm that the shuttle is ready for takeoff");
+        console.log(`The user selected: ${takeoffResponse}`);
+    });
 });
+
